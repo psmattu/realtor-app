880d2d9414e01fcc220fa71733041ec38a65e29d
psmattu <mattu.parteek@gmail.com>
2024-09-11 12:13:53 -0700

    Sign in page functionality and sign up variable fix

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -2,13 +2,13 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
-  const [formData, setFromData] = useState({});
+  const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
   const handleChange = (e) => {
-    setFromData({
+    setFormData({
       ...formData,
       [e.target.id]: e.target.value,
     });
